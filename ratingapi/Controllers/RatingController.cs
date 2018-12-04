b397d09003dfbde53b78e92945e902426974271c
bharathkumar <bharathkumar.jayaraman@wabco-auto.com>
2018-12-04 20:57:19 +0530

    RatingsController Added

---
+++ ratingapi/Controllers/RatingController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ratingapi.Controllers
+{
+    public class RatingController : Controller
+    {
+
+        // GET: Rating
+        public ActionResult GetRating()
+        {
+            Byte[] b = System.IO.File.ReadAllBytes(@"Test.jpg");
+            return File(b, "image/jpeg");
+        }
+
+        // GET: Rating
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Rating/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Rating/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Rating/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Rating/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Rating/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Rating/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Rating/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
